31a123ae99827572384a61d66d3de6c7fe9b05c3
gnioco <nejc.likar@gmail.com>
2023-12-16 23:25:46 +0200

    just doing it

---
+++ src/detector.py
@@ -0,0 +1,177 @@
+import sys
+import time
+import argparse
+import cv2
+
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+
+from multiprocessing import Process
+from multiprocessing import Queue
+
+from utils import visualize
+from utils import localize
+from PIL import Image
+from log import logger
+
+# Global variables to calculate FPS
+COUNTER, FPS = 0, 0
+START_TIME = time.time()
+
+def run(model: str, max_results: int, score_threshold: float, 
+        camera_id: int, width: int, height: int, show: bool) -> None:
+    """Continuously run inference on images acquired from the camera.
+
+    Args:
+    model: Name of the TFLite object detection model.
+    max_results: Max number of detection results.
+    score_threshold: The score threshold of detection results.
+    camera_id: The camera id to be passed to OpenCV.
+    width: The width of the frame captured from the camera.
+    height: The height of the frame captured from the camera.
+    """
+
+    # Start capturing video input from the camera or file (testing)
+    # cap = cv2.VideoCapture(camera_id)
+    cap = cv2.VideoCapture("Test_2.mp4")
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+
+    logger.info(f"[info] W, H, FPS\n{frameWidth}, {frameHeight}, {cap.get(cv2.CAP_PROP_FPS)}")
+
+    # Visualization parameters
+    row_size = 50  # pixels
+    left_margin = 24  # pixels
+    text_color = (0, 0, 0)  # black
+    font_size = 1
+    font_thickness = 1
+    fps_avg_frame_count = 10
+
+    detection_frame = None
+    detection_result_list = []
+
+
+    # initialize the input queue (frames)
+    # and the list of actual detections returned by the child process
+    inputQueue = Queue(maxsize=1)
+    img = None
+
+    # construct a child process *indepedent* from our main process of
+    # execution
+    logger.info("Starting process...")
+    p = Process(target=record_frame, args=(img, inputQueue))
+    p.daemon = True
+    p.start()
+    time.sleep(10)
+    logger.info("Starting capture...")
+    
+    def save_result(result: vision.ObjectDetectorResult, unused_output_image: mp.Image, timestamp_ms: int):
+        global FPS, COUNTER, START_TIME
+
+        # Calculate the FPS
+        if COUNTER % fps_avg_frame_count == 0:
+            FPS = fps_avg_frame_count / (time.time() - START_TIME)
+            START_TIME = time.time()
+
+        detection_result_list.append(result)
+        COUNTER += 1
+
+    # Initialize the object detection model
+    base_options = python.BaseOptions(model_asset_path=model)
+    options = vision.ObjectDetectorOptions(base_options=base_options,
+                                            running_mode=vision.RunningMode.LIVE_STREAM,
+                                            max_results=max_results, score_threshold=score_threshold,
+                                            result_callback=save_result)
+    detector = vision.ObjectDetector.create_from_options(options)
+
+
+    # Continuously capture images from the camera and run inference
+    while cap.isOpened():
+        success, frame = cap.read()
+        if not success:
+            sys.exit(
+                'ERROR: Unable to read from webcam. Please verify your webcam settings.'
+            )
+
+        # Capture frame-by-frame
+        # frame = frame.array
+        img = Image.fromarray(frame)
+        # if the input queue *is* empty, give the current frame to
+        # record_frame
+        if inputQueue.empty():
+            inputQueue.put(frame)
+
+        image = cv2.flip(frame, 1)
+
+        # Convert the image from BGR to RGB as required by the TFLite model.
+        rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_image)
+
+        # Run object detection using the model.
+        detector.detect_async(mp_image, time.time_ns() // 1_000_000)
+
+        # Show the FPS
+        fps_text = 'FPS = {:.1f}'.format(FPS)
+        text_location = (left_margin, row_size)
+        current_frame = image
+        cv2.putText(current_frame, fps_text, text_location, cv2.FONT_HERSHEY_DUPLEX,
+                    font_size, text_color, font_thickness, cv2.LINE_AA)
+
+        if detection_result_list:
+            # print(detection_result_list)
+            diver_location_list = localize(detection_result_list[0])
+            if (show):
+                current_frame = visualize(current_frame, detection_result_list[0])
+                detection_frame = current_frame
+                if detection_frame is not None:
+                    cv2.namedWindow('object_detection', cv2.WINDOW_NORMAL)
+                    cv2.resizeWindow('object_detection', frameWidth, frameHeight)
+                    cv2.imshow('object_detection', detection_frame)
+           
+
+            detection_result_list.clear()
+
+
+
+        # Stop the program if the ESC key is pressed.
+        if cv2.waitKey(1) == 27:
+            break
+
+    p.join()
+    detector.close()
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+def record_frame(img, inputQueue):
+    while True:
+        # check to see if there is a frame in our input queue
+        if not inputQueue.empty():
+            # grab the frame from the input queue
+            img = inputQueue.get()
+            # write frame to file
+
+import configparser
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cfg', help='path to cfg file', default="config.cfg")
+    config = configparser.ConfigParser()
+
+    # Load the configuration file
+    args = parser.parse_args()
+    config.read(args.cfg)
+    modelPath = config["detector"]["model_path"]
+    camera_idx = config["detector"]["cameraId"]
+    maxResults = config["detector"]["maxResults"]
+    confThreshold = config["detector"].getfloat("scoreThreshold")
+    frameWidth = config["detector"].getfloat("frameWidth")
+    frameHeight = config["detector"].getfloat("frameHeight")
+    show = config["general"].getboolean("show")
+    enable_motor = config["motor"].getboolean('enable')
+
+    run(modelPath, maxResults, confThreshold, 
+        camera_idx, frameWidth, frameHeight, 
+        show, enable_motor)
+    
---
+++ src/utils.py
@@ -0,0 +1,97 @@
+# Copyright 2023 The MediaPipe Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import cv2
+import numpy as np
+
+from lib.KalmanFilter import KalmanFilter
+
+MARGIN = 10  # pixels
+ROW_SIZE = 30  # pixels
+FONT_SIZE = 1
+FONT_THICKNESS = 1
+TEXT_COLOR = (0, 0, 0)  # black
+
+
+#Create KalmanFilter object KF
+#KalmanFilter(dt, u_x, u_y, std_acc, x_std_meas, y_std_meas)
+KF = KalmanFilter(0.1, 1, 1, 1, 0.1, 0.1)
+
+
+def visualize(image, detection_result) -> np.ndarray:
+  """Draws bounding boxes on the input image and return it.
+  Args:
+    image: The input RGB image.
+    detection_result: The list of all "Detection" entities to be visualized.
+  Returns:
+    Image with bounding boxes.
+  """
+  for detection in detection_result.detections:
+    # Draw bounding_box
+    bbox = detection.bounding_box
+    start_point = bbox.origin_x, bbox.origin_y
+    end_point = bbox.origin_x + bbox.width, bbox.origin_y + bbox.height
+    # Use the orange color for high visibility.
+    cv2.rectangle(image, start_point, end_point, (0, 165, 255), 3)
+
+    # Draw label and score
+    category_name = detection.categories[0].category_name
+    probability = round(detection.categories[0].score, 2)
+    result_text = category_name + ' (' + str(probability) + ')'
+    text_location = (MARGIN + bbox.origin_x,
+                     MARGIN + ROW_SIZE + bbox.origin_y)
+    cv2.putText(image, result_text, text_location, cv2.FONT_HERSHEY_DUPLEX,
+                FONT_SIZE, TEXT_COLOR, FONT_THICKNESS, cv2.LINE_AA)
+
+  return image
+
+def localize(image, detection_result) -> np.ndarray:
+  
+  """Gets locations of divers on the input image and return it.
+  Args:
+    detection_result: The list of all "Detection" entities.
+  Returns:
+    Array with diver locations.
+  """
+  diver_location_list = []
+  center = None
+
+  # function to find deepest diver
+  def find_deepest_diver(tuple_list):
+    if not tuple_list:
+        return None  # Return None for an empty list
+
+    min_tuple = min(tuple_list, key=lambda x: x[1])
+    return min_tuple
+  
+  # add divers centers to the list
+  for detection in detection_result.detections:    
+        if detection.categories[0].category_name is "diver":     
+            bbox = detection.bounding_box
+            diver_xC = bbox.origin_x + bbox.width/2, bbox.origin_y - bbox.height/2
+            diver_location_list.append(diver_xC)
+  
+  # get location of the deepest detected diver
+  diver_location = find_deepest_diver(diver_location_list)
+  # Predict
+  (x, y) = KF.predict()                
+  # Update
+  (x1, y1) = KF.update(center)                    
+  center = [int(x1[0,0]), int(x1[0,1])]
+
+    # Use the orange color for high visibility.
+  cv2.rectangle(image, start_point, end_point, (0, 165, 255), 3)
+
+
+  return diver_location_list
