f573b8286b913da655cd3c4f5a18b61f8f47966e
gnioco <nejc.likar@gmail.com>
2023-12-18 21:46:33 +0200

    dsa

--- src/servertest.py
+++ src/servertest.py
@@ -103,15 +103,17 @@ try:
     server = StreamingServer(address, StreamingHandler)
     server.serve_forever()
 
- #   picam2.start_encoder(encoder_rec, output_rec, quality=Quality.HIGH)
- #   time.sleep(5)
- #   picam2.stop_encoder(encoder_rec)
-finally:
-  picam2.stop_recording()
+    picam2.start_encoder(encoder_rec, output_rec, quality=Quality.HIGH)
+    time.sleep(5)
+    picam2.stop_encoder(encoder_rec)
+except:
+    print("ERROOOORRRR")
+  
 
 while True:
     im = picam2.capture_array()
 
     cv2.imshow("Camera", im)
     if cv2.waitKey(1) == 27:
+        picam2.stop_recording()
         break
