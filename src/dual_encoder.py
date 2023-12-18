c7627e509ad1f5415d666c6ed5d69060f75fce42
gnioco <nejc.likar@gmail.com>
2023-12-18 22:29:22 +0200

    fsfaf

--- src/dual_encoder.py
+++ src/dual_encoder.py
@@ -1,19 +1,22 @@
-#!/usr/bin/python3
-import time
-
 from picamera2 import Picamera2
-from picamera2.encoders import H264Encoder, MJPEGEncoder
-
+from picamera2.encoders import H264Encoder
+from picamera2.outputs import FileOutput, FfmpegOutput
+import time
 picam2 = Picamera2()
-video_config = picam2.create_video_configuration(main={"size": (1280, 720), "format": "RGB888"},
-                                                 lores={"size": (640, 480), "format": "YUV420"})
-
+video_config = picam2.create_video_configuration()
 picam2.configure(video_config)
-
-encoder1 = H264Encoder(10000000)
-encoder2 = MJPEGEncoder(10000000)
-
-picam2.start_recording(encoder1, 'test1.h264')
-picam2.start_recording(encoder2, 'test2.mjpeg', name="lores")
-time.sleep(10)
-picam2.stop_recording()
+encoder = H264Encoder(repeat=True, iperiod=15)
+output1 = FfmpegOutput("-f mpegts udp://192.168.117.121:12345")
+output2 = FileOutput()
+encoder.output = [output1, output2]
+# Start streaming to the network.
+picam2.start_encoder(encoder)
+picam2.start()
+time.sleep(5)
+# Start recording to a file.
+output2.fileoutput = "test.h264"
+output2.start()
+time.sleep(5)
+output2.stop()
+# The file is closed, but carry on streaming to the network.
+time.sleep(9999999)
