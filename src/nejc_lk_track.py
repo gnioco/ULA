bee3d7f5e4dbd9cc463be69fba274656d613d7a1
gnioco <nejc.likar@gmail.com>
2023-12-24 23:38:26 +0200

    dasda

--- src/nejc_lk_track.py
+++ src/nejc_lk_track.py
@@ -153,6 +153,7 @@ class App:
                         # Returns a contiguous flattened array as (x, y) coordinates for new point
                         a, b = new.ravel()
                         c, d = old.ravel()
+                        print(a)
                         distance = np.sqrt((a-c)**2 + (b-d)**2)
                         # distance between new and old points should fall within
                         # specific values for 2 points to be same the object
