4321ed5047ea891647e986f72471d0d6416513cc
gnioco <nejc.likar@gmail.com>
2023-12-24 22:09:23 +0200

    fee

--- src/nejc_lk_track.py
+++ src/nejc_lk_track.py
@@ -161,11 +161,14 @@ class App:
                 if detection_result_list:
                     # print(detection_result_list)
                     diver_location = localize(detection_result_list[0])
+
+                    if diver_location is None:
+                        diver_location=[0,0]
+                        
                     if len(self.tracks) == 0:
                         self.tracks.append([(diver_location[0], diver_location[1])])
                     cv.circle(image, [diver_location[0], diver_location[1]], 10, (0, 255, 0), 5)
-                    if diver_location is None:
-                        diver_location=[0,0]
+                    
 
                     detection_result_list.clear()
                 
