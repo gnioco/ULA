c71f2749cdc1cff4fb4def055399c5a40536d076
gnioco <nejc.likar@gmail.com>
2023-12-26 23:42:09 +0100

    dadsa

--- src/nejc_lk_track.py
+++ src/nejc_lk_track.py
@@ -84,7 +84,7 @@ class Tracker:
         self.track_len = 10
         self.detect_interval = 10
         self.tracks = []
-        self.cam = cv.VideoCapture("../ula/test/Test_2.mp4")
+        self.cam = cv.VideoCapture("../ula/test/Test_2.mp4Mylena4'.m4v")
         self.cam.set(cv.CAP_PROP_FRAME_WIDTH, self.frameWidth)
         self.cam.set(cv.CAP_PROP_FRAME_HEIGHT, self.frameHeight)
         self.frame_idx = 0
