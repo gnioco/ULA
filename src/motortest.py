27755516fd1c12f1439d638b1dd83c4bbf7a8f7d
gnioco <nejc.likar@gmail.com>
2024-01-01 14:36:07 +0100

    fafa

--- src/motortest.py
+++ src/motortest.py
@@ -41,7 +41,7 @@ EN_pin.off()
 
 mymotortest.motor_go(False, # True=Clockwise, False=Counter-Clockwise
                      "Full" , # Step type (Full,Half,1/4,1/8,1/16,1/32)
-                     200, # number of steps
+                     20000, # number of steps
                      .005, # step delay [sec]
                      False, # True = print verbose output 
                      .05) # initial delay [sec]
